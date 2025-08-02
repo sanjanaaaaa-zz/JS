a13dd50f2fb1c65d33eb03fc28f0c1a6aad76b8d
sanjanaaaaa-zz <sanjanaec47@gmail.com>
2025-08-02 06:31:03 +0000

    work dome for morning

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,3 @@
+"use strict"; //treat all JS code as newwer version
+
+// alert(2+2)  We're using Nodejs not browser
--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -2,7 +2,17 @@ const accountId = 12345678  //cont ka mtlb iski value change nhi hoti
 let accountEmail = "s@Email.com"
 var accountPassword = "1234"
 accountCity = "Delhi"
+let accountState;
 
-accountId=123
+// accountId=123
+
+accountEmail = "sanjana@Email.com"
+accountPassword = "12344321"
+accountCity = "Bengal"
+
+/*
+Prefer Not to use var because of issue in block scope and functional scope
+*/
 
 console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
