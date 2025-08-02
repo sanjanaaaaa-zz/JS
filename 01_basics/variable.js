730ba3c0f6a7e220c72b66b9349295d14e19e1ef
sanjanaaaaa-zz <sanjanaec47@gmail.com>
2025-08-02 06:04:29 +0000

    variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,8 @@
+const accountId = 12345678  //cont ka mtlb iski value change nhi hoti
+let accountEmail = "s@Email.com"
+var accountPassword = "1234"
+accountCity = "Delhi"
+
+accountId=123
+
+console.log(accountId);
