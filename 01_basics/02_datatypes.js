decdc0ce97cb8bdb58229c3b1f36077c890d213a
sanjanaaaaa-zz <sanjanaec47@gmail.com>
2025-08-03 06:51:52 +0000

    Datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,19 @@
 "use strict"; //treat all JS code as newwer version
-
+//code readability should be high
 // alert(2+2)  We're using Nodejs not browser
+let name="Sanjana"
+let age=21
+let isLoggedIn=true
+
+console.log(name)
+
+// Data type is JS
+// number
+// string
+// boolean
+// null
+// undefined
+// symbol => unique value
+
+console.log(typeof null)
+console.log(typeof undefined)
