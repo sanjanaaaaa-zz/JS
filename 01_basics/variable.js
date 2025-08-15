b8f859863f06fda82ff69395881acb44bc80d4f8
sanjanaaaaa-zz <sanjanaec47@gmail.com>
2025-08-15 05:16:58 +0000

    Done some rev.

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -4,15 +4,18 @@ var accountPassword = "1234"
 accountCity = "Delhi"
 let accountState;
 
-// accountId=123
+// accountId=123 //Assignment to Const keyword
 
 accountEmail = "sanjana@Email.com"
 accountPassword = "12344321"
 accountCity = "Bengal"
 
 /*
-Prefer Not to use var because of issue in block scope and functional scope
+Prefer Not to use var 
+because of issue in block scope and functional scope
 */
 
 console.log(accountId);
+
+// Ek saath sbko print krne k liye Use Console.table() ;
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
